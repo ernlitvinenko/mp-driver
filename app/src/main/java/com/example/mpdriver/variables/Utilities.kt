5db94b145bd9d028e74796af2f898e42a6207fd1
Ernest Litvinenko <ernest@elitvinenko.tech>
2025-04-15 13:55:36 +0300

    Update version name and ver code

--- app/src/main/java/com/example/mpdriver/variables/Utilities.kt
+++ app/src/main/java/com/example/mpdriver/variables/Utilities.kt
@@ -7,8 +7,8 @@ import kotlinx.datetime.format.byUnicodePattern
 import kotlinx.datetime.format.char
 import java.time.format.DateTimeFormatter
 
-val version = "0.0.2alpha"
-val VC = 21
+val version = "0.0.2beta1"
+val VC = 22
 
 class PersonalDatetimeFormat(private val pattern: String) {
     fun toJava(): DateTimeFormatter {
